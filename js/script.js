35ef8112a3a31feda8e2f09e14295abba088fd7b
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-16 15:57:53 +0200

    image error

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log("Vue ok", Vue);
+
+const { createApp } = Vue;
+
+const app = createApp({
+  data() {
+    return {
+      title: "ci si diverte con Vue",
+      image: "neom-QZGW6t70IsI-unsplash.jpg",
+    };
+  },
+});
+
+app.mount("#root");
